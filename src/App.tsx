819a4a66a51ffe2a0f0206a75daac49eaad79a99
chrishson <chris.h.son@gmail.com>
2022-06-20 14:39:00 -0700

    auth0/cleanup: added basic route protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-
-import './App.css';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Login from './components/Login';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Main } from './components/Main';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <>
       <header>
         <NavBar />
       </header>
       <Routes>
-        {/* <Route path="/" exact component={Home} />
-        <AuthenticatedRoute path="/profile" component={Profile} />
-        <AuthenticatedRoute path="/people" component={People} /> */}
+        <Route path="/" element={isAuthenticated ? <Main /> : <Login />} />
       </Routes>
     </>
   );
