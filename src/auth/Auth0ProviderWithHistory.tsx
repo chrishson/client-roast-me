f7a7b9c4170edf2fd51c2edcdcab86ab9e4c9bf6
chrishson <chris.h.son@gmail.com>
2022-06-20 14:05:59 -0700

    Auth0/Cleanup: Gave better typing

--- src/auth/Auth0ProviderWithHistory.tsx
+++ src/auth/Auth0ProviderWithHistory.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+import { AppState, Auth0Provider } from '@auth0/auth0-react';
 
-const Auth0ProviderWithHistory = ({ children }: any) => {
+interface Auth0ProviderWithHistoryProps {
+    children: JSX.Element
+}
+
+const Auth0ProviderWithHistory = ({ children }: Auth0ProviderWithHistoryProps) => {
     const domain = process.env.REACT_APP_AUTH0_DOMAIN as string;
     const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID as string;
 
     const navigate = useNavigate();
 
-    const onRedirectCallback = (appState: any) => {
+    const onRedirectCallback = (appState?: AppState) => {
         navigate(appState?.returnTo || window.location.pathname);
     };
 
