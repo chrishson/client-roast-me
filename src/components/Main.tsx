e606b9165d20ce61c573cd9e9129486ffaca0419
chrishson <chris.h.son@gmail.com>
2022-06-22 15:59:48 -0700

    auth0/commit-to-api: setup simple test to pass auth token to api

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,26 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
+
 export function Main() {
-    return <div> This is a protected Page</div>
+    const { getAccessTokenSilently } = useAuth0();
+    const [data, setData] = useState({ message: '' });
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const token = await getAccessTokenSilently();
+                const response = await fetch('http://localhost:3010/api/private', {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                setData(await response.json());
+            } catch (e) {
+                console.error(e);
+            }
+        })();
+    }, [getAccessTokenSilently]);
+
+    return <div> {data?.message} </div>
 }
